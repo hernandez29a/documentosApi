d67bdb5e48d57a2da8d939e157ddbe270a496f8e
Gustavo Hernandez <hernandez29a@gmail.com>
2023-03-22 17:36:40 -0400

    wip entidad de documento

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from '../common/common.module';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './entities/document.entity';
 
 @Module({
   controllers: [DocumentController],
-  providers: [DocumentService]
+  providers: [DocumentService],
+  imports: [
+    ConfigModule,
+    CommonModule,
+    AuthModule,
+    TypeOrmModule.forFeature([Document]),
+  ],
+  exports: [],
 })
 export class DocumentModule {}
--- src/document/entities/document.entity.ts
+++ src/document/entities/document.entity.ts
@@ -1,1 +1,33 @@
-export class Document {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({ name: 'documentos' })
+export class Document {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: true,
+  })
+  titleDocument: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'bool',
+    default: true,
+  })
+  isActive: boolean;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  image: string;
+
+  // TODO relaciones con subcategorias
+}
